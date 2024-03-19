376254464cd392ca74c0fee233dd0a2fb0b33ee9
Denis Zhelaev <125196493+zhelaevdenis@users.noreply.github.com>
2024-03-19 11:23:16 +0300

    Add files via upload

---
+++ lw5Folder/OriginalApp/lw5OrigApp/lw5OrigApp.cpp
@@ -0,0 +1,98 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+#include "tchar.h"
+#include <fstream>
+#include <chrono>
+
+CRITICAL_SECTION FileLockingCriticalSection;
+
+int ReadFromFile() {
+	EnterCriticalSection(&FileLockingCriticalSection);
+	std::fstream myfile("balance.txt", std::ios_base::in);
+	int result;
+	myfile >> result;
+	myfile.close();
+	LeaveCriticalSection(&FileLockingCriticalSection);
+
+	return result;
+}
+
+void WriteToFile(int data) {
+	EnterCriticalSection(&FileLockingCriticalSection);
+	std::fstream myfile("balance.txt", std::ios_base::out);
+	myfile << data << std::endl;
+	myfile.close();
+	LeaveCriticalSection(&FileLockingCriticalSection);
+}
+
+int GetBalance() {
+	int balance = ReadFromFile();
+	return balance;
+}
+
+void Deposit(int money) {
+	int balance = GetBalance();
+	balance += money;
+
+	WriteToFile(balance);
+	printf("Balance after deposit: %d\n", balance);
+}
+
+void Withdraw(int money) {
+	if (GetBalance() < money) {
+		printf("Cannot withdraw money, balance lower than %d\n", money);
+		return;
+	}
+
+	Sleep(20);
+	int balance = GetBalance();
+	balance -= money;
+	WriteToFile(balance);
+	printf("Balance after withdraw: %d\n", balance);
+}
+
+DWORD WINAPI DoDeposit(CONST LPVOID lpParameter)
+{
+	Deposit((int)lpParameter);
+	ExitThread(0);
+}
+
+DWORD WINAPI DoWithdraw(CONST LPVOID lpParameter)
+{
+	Withdraw((int)lpParameter);
+	ExitThread(0);
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	HANDLE* handles = new HANDLE[49];
+	InitializeCriticalSection(&FileLockingCriticalSection);
+
+	auto start_time = std::chrono::high_resolution_clock::now();
+
+	WriteToFile(0);
+	SetProcessAffinityMask(GetCurrentProcess(), 1);
+	for (int i = 0; i < 50; i++) {
+		handles[i] = (i % 2 == 0)
+			? CreateThread(NULL, 0, &DoDeposit, (LPVOID)230, CREATE_SUSPENDED, NULL)
+			: CreateThread(NULL, 0, &DoWithdraw, (LPVOID)1000, CREATE_SUSPENDED, NULL);
+		ResumeThread(handles[i]);
+	}
+
+
+	// ожидание окончания работы двух потоков
+	WaitForMultipleObjects(50, handles, true, INFINITE);
+	
+	auto end_time = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+	printf("Final Balance: %d\n", GetBalance());
+	std::cout << "Time: " << duration;
+
+	
+	getchar();
+
+	DeleteCriticalSection(&FileLockingCriticalSection);
+
+	return 0;
+}
