dc9316cbe95f0bc4fa9bea0766b261d893d0bcef
Denis Zhelaev <125196493+zhelaevdenis@users.noreply.github.com>
2024-03-19 11:41:59 +0300

    Add files via upload

---
+++ lw6/SynchApp/SynchApp/SynchApp.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+double calculatePi(int iterations) {
+    double pi = 0.0;
+    double sign = 1.0;
+    for (int i = 0; i < iterations; ++i) {
+        pi += sign / (2.0 * i + 1.0);
+        sign *= -1.0;
+    }
+    return pi * 4.0;
+}
+
+int main() {
+    const int iterations = 100000000;
+    clock_t start = clock();
+    double pi = calculatePi(iterations);
+    clock_t end = clock();
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
+    std::cout << "Pi: " << std::setprecision(15) << pi << std::endl;
+    std::cout << "Time: " << elapsed_secs << std::endl;
+
+    return 0;
+}
